895e006fe59014eb65e764a2f43e02ddac302f67
venkin84 <venkataraman.n.mail@gmail.com>
2021-08-24 09:43:55 +0530

    Initial commit

---
+++ src/main/java/com/venkin84/demomsconfigservice/DemoMsConfigserviceApplication.java
@@ -0,0 +1,13 @@
+package com.venkin84.demomsconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMsConfigserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMsConfigserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/venkin84/demomsconfigservice/DemoMsConfigserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkin84.demomsconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMsConfigserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
